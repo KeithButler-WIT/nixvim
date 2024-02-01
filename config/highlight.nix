cbeb51eef601f314ccae42ee638671a88c73140b
Keith Butler <keithbutler2001@gmail.com>
2024-02-01 10:45:29 +0000

    removed highlight comment colour

--- config/highlight.nix
+++ config/highlight.nix
@@ -1,7 +1,7 @@
 {
   highlight = {
-    Comment.fg = "#ff00ff";
-    Comment.bg = "#000000";
+    # Comment.fg = "#ff00ff";
+    # Comment.bg = "#000000";
     Comment.underline = true;
     Comment.bold = true;
   };
